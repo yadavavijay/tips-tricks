3e14e441e43c199c86d9745b10aac97d1ecb7968
abhimanyu <abhimanyu@ABHIMANYUI5>
2017-01-12 16:46:15 +0530

    bundle and minification and cdn demo added

---
+++ BundlingAndMinificationMVC/App_Start/BundleConfig.cs
@@ -0,0 +1,83 @@
+﻿using System.Configuration;
+using System.Web.Optimization;
+
+namespace BundlingAndMinificationMVC
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // use in-memory bundle
+            string usebundle = ConfigurationManager.AppSettings["usedundle"];
+            if (usebundle == "true")
+            {
+                BundleTable.EnableOptimizations = true;  // OR <compilation debug="false" targetFramework="4.5" /> on production
+            }
+
+
+            // use cdn bundle files
+            string usedundlecdn = ConfigurationManager.AppSettings["usedundlecdn"];
+            if (usedundlecdn == "true")
+            {
+                bundles.UseCdn = true;
+            }
+
+
+            // cdn bundle details and version control
+            string cachecdnbundleversion = ConfigurationManager.AppSettings["cachecdnbundleversion"];
+            var version = System.Reflection.Assembly.GetAssembly(typeof(Controllers.HomeController)).GetName().Version.ToString();
+            var cdnUrl = "http://cdn-yourwebsite.com/knorish-static-assets/bundles/{0}?" + version + "-" + cachecdnbundleversion;
+
+
+            // bundling
+            bundles.Add(new ScriptBundle("~/bundles/jquery", string.Format(cdnUrl, "jquery.js")).Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval", string.Format(cdnUrl, "jqueryval.js")).Include(
+                        "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr", string.Format(cdnUrl, "modernizr.js")).Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", string.Format(cdnUrl, "bootstrap.js")).Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/content/css", string.Format(cdnUrl, "site.css")).Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+
+
+            // Further steps: Generate bundle files offline (not in-memory) from http://localhost:41667/generatebundle/execute and upload on CDN storage
+
+
+
+
+
+
+
+
+
+
+
+
+            // Default code
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/respond.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
+        }
+    }
+}
---
+++ BundlingAndMinificationMVC/Controllers/GenerateBundleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+
+namespace BundlingAndMinificationMVC.Controllers
+{
+    //[Authorize] //only authorized users can generate bundle
+    public class GenerateBundleController : Controller
+    {
+        public string Execute()
+        {
+            string urlPrefix = Request.Url.GetLeftPart(UriPartial.Authority);
+
+            var bundlePaths = new List<KeyValuePair<string, string>>();
+
+            bundlePaths.Add(new KeyValuePair<string, string>("/bundles/jquery", "jquery.js"));
+            bundlePaths.Add(new KeyValuePair<string, string>("/bundles/jqueryval", "jqueryval.js"));
+            bundlePaths.Add(new KeyValuePair<string, string>("/bundles/modernizr", "modernizr.js"));
+            bundlePaths.Add(new KeyValuePair<string, string>("/bundles/bootstrap", "bootstrap.js"));
+            bundlePaths.Add(new KeyValuePair<string, string>("/content/css", "site.css"));
+
+            foreach (var item in bundlePaths)
+            {
+                string downloadFileAt = Server.MapPath("~/") + "scripts/dynamicbundles/" + item.Value;
+                WebClient myWebClient = new WebClient();
+                myWebClient.DownloadFile(new Uri(urlPrefix + item.Key), downloadFileAt);
+            }
+
+            return "Bungle generated at /scripts/dynamicbundles/, now upload them on production cdn.";
+        }
+    }
+}
