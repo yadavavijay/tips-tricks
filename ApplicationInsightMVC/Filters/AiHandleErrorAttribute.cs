a588d0f52cd3a214f58c6f512186c809d20e1bf0
itorian <itorian@live.in>
2017-02-18 21:10:28 +0530

    application insight updated

---
+++ ApplicationInsightMVC/Filters/AiHandleErrorAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Web.Mvc;
+
+namespace ApplicationInsightMVC.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true, AllowMultiple = true)]
+    public class AiHandleErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            if (filterContext != null && filterContext.HttpContext != null && filterContext.Exception != null)
+            {
+                if (filterContext.HttpContext.IsCustomErrorEnabled)
+                {
+                    var ai = new TelemetryClient();
+                    //ai.InstrumentationKey = ""; //configured this globally in Application_Start
+                    ai.TrackException(filterContext.Exception);
+                }
+            }
+            base.OnException(filterContext);
+        }
+    }
+}
--- ApplicationInsightMVC/Global.asax.cs
+++ ApplicationInsightMVC/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.ApplicationInsights.Extensibility;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +14,8 @@ namespace ApplicationInsightMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            TelemetryConfiguration.Active.InstrumentationKey = WebConfigurationManager.AppSettings["iKey"];
         }
     }
 }
